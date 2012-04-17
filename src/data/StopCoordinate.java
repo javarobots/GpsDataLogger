ca05e2a4fa4cd853ef39ad46c24e7e15b51ee65e
javarobots74 <javarobots74@gmail.com>
2012-04-17 07:09:04 -0600

    Ensure month and day are two digits

--- src/data/StopCoordinate.java
+++ src/data/StopCoordinate.java
@@ -10,48 +10,48 @@ import org.joda.time.DateTime;
  *  <TimeStamp>
  *       <when>1997-07-16T07:30:15Z</when>
  *  </TimeStamp>
- * 
+ *
  * @author javarobots
  */
 public class StopCoordinate extends Coordinate {
-    
+
     private String mStopTime;
-    
+
     public StopCoordinate(){
         super();
     }
-    
-    public void markStopTime(){        
+
+    public void markStopTime(){
         StringBuilder whenBuilder = new StringBuilder();
         NumberFormat formatter = NumberFormat.getInstance();
         formatter.setMinimumIntegerDigits(2);
-        
+
         //Get current date and time and adjust to UTC
         DateTime timeInstance = new DateTime();
         int rawOffset = timeInstance.getZone().toTimeZone().getRawOffset();
-        timeInstance = timeInstance.plusMillis(rawOffset);     
-        
+        timeInstance = timeInstance.plusMillis(rawOffset);
+
         //Get and build the date
         String year = timeInstance.year().getAsString();
-        String month = timeInstance.monthOfYear().getAsString();
-        String day = timeInstance.dayOfMonth().getAsString();
+        String month = formatter.format(Integer.parseInt(timeInstance.monthOfYear().getAsString())).toString();
+        String day = formatter.format(Integer.parseInt(timeInstance.dayOfMonth().getAsString())).toString();
         whenBuilder.append(year).append("-").append(month).append("-").append(day);
-        
+
         //Add time separator
         whenBuilder.append("T");
-        
+
         //Get and build the UTC time
         int hour = timeInstance.getHourOfDay();
         int minute = timeInstance.getMinuteOfHour();
-        int second = timeInstance.getSecondOfMinute();        
+        int second = timeInstance.getSecondOfMinute();
         whenBuilder.append(formatter.format(hour)).append(":").append(formatter.format(minute)).append(":").append(formatter.format(second)).append("Z");
-        
+
         //Set the time
-        mStopTime = whenBuilder.toString();       
+        mStopTime = whenBuilder.toString();
     }
-    
+
     public String getStopTime(){
         return mStopTime;
     }
-    
+
 }
